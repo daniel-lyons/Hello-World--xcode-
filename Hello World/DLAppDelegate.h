9d37699e0cd84831a0ae63a7fbc112d847a67573
daniel-lyons <daniellyons@Daniel-Lyons-Computer.local>
2013-10-12 04:55:10 -1000

    Initial Commit

---
+++ Hello World/DLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLAppDelegate.h
+//  Hello World
+//
+//  Created by Daniel Lyons on 10/12/13.
+//  Copyright (c) 2013 Daniel Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
